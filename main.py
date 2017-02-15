86ee9d8c46fa0096876dbc68857db2881c00ff6f
KatteKurach <kattekurach@gmail.com>
2017-02-15 16:53:52 +0300

    Main architecture created.

---
+++ main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+class Task(object):
+    pass
+
+def addPoisonPill():
+    pass
+
+def readInputFile():
+    pass
+
+def writeLine(file, line):
+    pass
+
+def allowed(url):
+    pass
+
+def getCrawlDelay(url):
+    pass
+
+def threadLoop(queue, condition):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
