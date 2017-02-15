70129ffb0323500c15adacc1e26b1180549dba9d
KatteKurach <kattekurach@gmail.com>
2017-02-15 18:01:13 +0300

    Thread logic added.

--- main.py
+++ main.py
@@ -1,28 +1,73 @@
 # -*- coding: utf-8 -*-
 
+import csv
+import urllib.robotparser as pr
+import threading
+import Queue
+
+
 class Task(object):
+    def __init__(self, func, args):
+        self.func = func
+        self.args = args[:]
+        
+
+    def run(self):
+        self.func(*self.args)
+
+
+def addPoisonPill(queue, threadCount):
+    for _ in range(threadCount):
+        queue.put(None)
+
+
+def processUrl(url, writer, writerLock):
     pass
 
-def addPoisonPill():
+def readQueueFromFile(filename, queue):
+    with open(filename, 'r') as table:
+        reader = csv.reader(table)
+        for line in reader:
+            queue.put(Task(processUrl, [line[0]])
+
+
+def writeLine(writer, line):
     pass
 
-def readInputFile():
-    pass
-
-def writeLine(file, line):
-    pass
-
-def allowed(url):
-    pass
 
 def getCrawlDelay(url):
     pass
 
-def threadLoop(queue, condition):
+def threadLoop(queue, condition, writer, writerLock):
     pass
 
-def main():
+def delayLoop(delayCondition, delay, threads):
     pass
+
+
+def main():
+    inputFile = 'wikipedia_links.csv' 
+    outputFile = 'temp.csv'
+    threadCount = 4
+
+    delayCondition = threading.Condition(threading.Lock())
+    writerLock = threading.Lock()
+    queue = Queue.Queue()
+   
+    threads = []
+    f = open(output)
+    writer = csv.writer(f, delimeter = ',')
+
+    for _ in range(threadCount):
+        thread = threading.Thread(target = threadLoop, args = (queue, delayCondition, writer, writerLock))
+        thread.start()
+        threads.append(thread)
+    delayManager = threading.Thread(target = delayLoop, args = (delayCondition, delay, threads))
+    delayManager.start()
+
+    getQueueFromFile(inputFile, queue)
+    delayManager.join()
+    f.close()
 
 if __name__ == '__main__':
     main()
